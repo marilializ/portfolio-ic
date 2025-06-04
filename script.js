93ff52a713137fc5298b7ef2e5698ea193ccb911
marilializ <lizmarilia07@gmail.com>
2025-06-04 11:04:15 -0300

    finalizando css e add js

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.style.opacity = 0;
+  setTimeout(() => {
+    document.body.style.transition = "opacity 1s";
+    document.body.style.opacity = 1;
+  }, 100);
+});
